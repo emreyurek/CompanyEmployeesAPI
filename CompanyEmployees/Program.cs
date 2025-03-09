bff886ebd7c5498d8b6f28f969f8a102211396bb
emreyurek <yurekemre407@gmail.com>
2025-03-10 02:33:30 +0300

    global error handling with UseExceptionHandler middleware

--- CompanyEmployees/Program.cs
+++ CompanyEmployees/Program.cs
@@ -1,5 +1,6 @@
 using CompanyEmployees;
 using CompanyEmployees.Extensions;
+using Contracts;
 using Microsoft.AspNetCore.HttpOverrides;
 using NLog;
 
@@ -25,14 +26,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
+var logger = app.Services.GetRequiredService<ILoggerManager>();
+app.ConfigureExceptionHandler(logger);
+
+if (app.Environment.IsProduction())
     app.UseHsts();
-}
 
 app.UseHttpsRedirection();
 
