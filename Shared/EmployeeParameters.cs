ad82faaa52c3065fd85b1d15016c674156e0c9f4
emreyurek <yurekemre407@gmail.com>
2025-03-20 03:52:02 +0300

    search by employee name

--- Shared/EmployeeParameters.cs
+++ Shared/EmployeeParameters.cs
@@ -7,5 +7,6 @@ namespace Shared
         public uint MinAge { get; set; }
         public uint MaxAge { get; set; } = int.MaxValue;
         public bool ValidAgeRange => MaxAge > MinAge;
+        public string? SearchTerm { get; set; }
     }
 }
